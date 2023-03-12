b9c21c23ceb3039c46cf46ae39e34247cba91f17
José Guilherme <josegfernandes03@gmail.com>
2023-03-12 17:17:03 -0300

    commit

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-_2ma1f73!(2qf&k-t2r++&c^-1^q@8izobn(9kx28@25!ygu_h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -82,12 +82,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "railway",
-        "USER": "root",
-        "PASSWORD": "kXv1nSbIZCUDD8O3LqIx",
-        "HOST": "containers-us-west-24.railway.app",
-        "PORT": "7755",
+        "USER": "postgres",
+        "PASSWORD": "hb0qh6ANV1tSpMsLvOVH",
+        "HOST": "containers-us-west-34.railway.app",
+        "PORT": "7891",
     }
 }
 
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
